632aa6f551d7968415014528b527158329b8dbdb
Liangchaoming <liangchaoming@yiqiniu.com>
2017-06-15 11:24:32 +0800

    添加ss入口

--- src/components/appbar.js
+++ src/components/appbar.js
@@ -6,25 +6,20 @@ import List from 'material-ui/List/List'
 import ListItem from 'material-ui/List/ListItem'
 import { openLink } from 'assets/utils/tool'
 import Account from 'material-ui/svg-icons/action/account-circle'
-import IconButton from 'material-ui/IconButton';
+import IconButton from 'material-ui/IconButton'
 import Home from 'material-ui/svg-icons/action/home'
 import Out from 'material-ui/svg-icons/maps/directions-run'
+import Send from 'material-ui/svg-icons/content/send'
 import { browserHistory } from 'react-router'
 import { fetchWithToken, apiURL } from 'assets/utils/request'
 import { logout } from 'modules/app_bar'
 
 import './appbar.less'
 
-
-
 class AppBarFixed extends React.Component {
   constructor (props) {
     super(props)
     this.state = {open: false}
-  }
-
-  titleCSS = {
-    textAlign: 'center'
   }
 
   handleToggle = () => this.setState({open: !this.state.open})
@@ -56,6 +51,19 @@ class AppBarFixed extends React.Component {
       .catch(err => console.log('登出问题:' + err))
   }
 
+  _gotoSSPage = () => {
+    this.handleClose()
+    browserHistory.push('/ss_page')
+  }
+
+  renderSSbtn = (isLogin) => (
+    isLogin ?
+    <div className="login-btn ss-page" onTouchTap={this._gotoSSPage}>
+     <Send className="login-icon"/>
+    </div>
+    : null
+  )
+
   render () {
     const {isLogin, appTitle} = this.props
 
@@ -63,7 +71,7 @@ class AppBarFixed extends React.Component {
       <div>
         <div className='fixed-nav'>
           <AppBar
-            title={appTitle} titleStyle={this.titleCSS}
+            title={appTitle} titleStyle={{textAlign: 'center'}}
             onLeftIconButtonTouchTap={this.handleToggle}
             onRightIconButtonTouchTap={this._goHome}
             iconElementRight={<IconButton><Home/></IconButton>}
@@ -76,12 +84,16 @@ class AppBarFixed extends React.Component {
                 docked={false}
                 width={280}>
 
-          {isLogin ? <div className="login-btn logout" onTouchTap={this._logout}>
-            <Out className="login-icon"/>
-          </div> : <div className="login-btn" onTouchTap={this._gotoLogin}>
-            <Account className="login-icon"/>
-          </div>}
+          {isLogin ?
+            <div className="login-btn logout" onTouchTap={this._logout}>
+              <Out className="login-icon"/>
+            </div>
+            :
+            <div className="login-btn" onTouchTap={this._gotoLogin}>
+               <Account className="login-icon"/>
+            </div>}
 
+          {this.renderSSbtn(isLogin)}
 
           <List className="about-me">
             <div className="author">Author:</div>
