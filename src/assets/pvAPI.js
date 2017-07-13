3779596f867f2f51875a300b18d62ed90e84c517
chaoming56 <1173306360@QQ.COM>
2017-07-13 22:11:29 +0800

    更爱api地址

--- src/assets/pvAPI.js
+++ src/assets/pvAPI.js
@@ -1,13 +1,13 @@
 const api = {
-  Get_pv: '/api/get_pv',
-  Record_pv: '/api/record_pv'
+    Get_pv: '/api/get_pv',
+    Record_pv: '/api/record_pv'
 }
 
-const host = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://123.207.34.114:4399';
+const host = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'http://123.207.34.114:3000';
 
 let pvAPI = {};
 
 for (let [key, value] of Object.entries(api)) {
-  pvAPI[key] = host + value;
+    pvAPI[key] = host + value;
 }
 export default pvAPI
